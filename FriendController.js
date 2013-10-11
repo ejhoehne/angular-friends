40a2c12df8f87665db7d78298541dd7affa99fa6
Emily <ejhoehne@gmail.com>
2013-10-10 20:53:32 -0600

    Begins project

--- FriendController.js
+++ FriendController.js
@@ -1,3 +1,15 @@
+app.controller('FriendController', function($scope)
+{
+	$.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(response){
+		$scope.friends= response.results;
+		console.log(response.results);
+	});
+
+//$scope.$apply(); --refreshes views in the scope
+
+
+});
+
 
 
 
